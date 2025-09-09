b12d8878e8fde1e6a7526b61ebbeb9fd4520b39f
akartikov <akartikov@avito.ru>
2025-09-09 13:17:27 +0300

    Добавил command для добавления места хранения для курьера

---
+++ internal/core/application/usecases/commands/add_storage_place_handler.go
@@ -0,0 +1,54 @@
+package commands
+
+import (
+	"context"
+	"delivery/internal/core/ports"
+	"delivery/internal/pkg/errs"
+	"errors"
+)
+
+type AddStoragePlaceHandler interface {
+	Handle(ctx context.Context, command AddStoragePlaceCommand) error
+}
+
+var _ AddStoragePlaceHandler = (*addStoragePlaceHandler)(nil)
+
+type addStoragePlaceHandler struct {
+	uowFactory ports.UnitOfWorkFactory
+}
+
+func NewAddStoragePlaceHandler(uowFactory ports.UnitOfWorkFactory) AddStoragePlaceHandler {
+	return &addStoragePlaceHandler{uowFactory: uowFactory}
+}
+
+func (h *addStoragePlaceHandler) Handle(ctx context.Context, command AddStoragePlaceCommand) error {
+	if !command.IsValid() {
+		return errs.NewCommandIsInvalidErrorWithCause(command.CommandName(), errors.New("should use NewAddStoragePlaceCommand to create a command"))
+	}
+
+	uow := h.uowFactory.NewUOW()
+
+	err := uow.Do(ctx, func(ctx context.Context) error {
+		courier, uowErr := uow.CourierRepo().Get(ctx, command.CourierID())
+		if uowErr != nil {
+			return uowErr
+		}
+
+		err := courier.AddStoragePlace(command.Name(), command.TotalVolume())
+		if err != nil {
+			return err
+		}
+
+		uowErr = uow.CourierRepo().Update(ctx, courier)
+		if uowErr != nil {
+			return uowErr
+		}
+
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
